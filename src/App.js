f4b6a21bdf7cc7d59a2c57fad2e418fa4b3c404a
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-03-08 21:28:41 +0700

    testing using props and usestate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { logo, iconHamburger } from "./images";
+import { useState } from "react";
+import Modal from "./components/Modal";
+
 function App() {
-  return <div></div>;
+  const [modal, setModal] = useState(false);
+  const handleClick = () => {
+    setModal(!modal);
+  };
+  return (
+    <>
+      <div>
+        {/* Header */}
+        <header className="flex justify-between p-5 pt-8 bg-gray-500">
+          {/* Logo and burger */}
+          <a href="/">
+            <img src={logo} alt="" />
+          </a>
+          <button onClick={handleClick}>
+            <img src={iconHamburger} alt="" />
+          </button>
+        </header>
+      </div>
+
+      <Modal modal={modal} handleClick={handleClick} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/images/index.js
@@ -0,0 +1,17 @@
+import iconBookmark from "./icon-bookmark.svg";
+import iconCheck from "./icon-check.svg";
+import iconCloseMenu from "./icon-close-menu.svg";
+import iconCloseModal from "./icon-close-modal.svg";
+import iconHamburger from "./icon-hamburger.svg";
+import logoMastercraft from "./logo-mastercraft.svg";
+import logo from "./logo.svg";
+
+export {
+  iconBookmark,
+  iconCheck,
+  iconCloseMenu,
+  iconCloseModal,
+  iconHamburger,
+  logoMastercraft,
+  logo,
+};

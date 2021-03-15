49a68737be28f8691692482ee501479d6cb9ab89
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2021-03-15 15:51:21 +0700

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-import { logo, iconHamburger } from "./images";
-import { useState } from "react";
-import Modal from "./components/Modal";
-
 function App() {
-  const [modal, setModal] = useState(false);
-  const handleClick = () => {
-    setModal(!modal);
-  };
   return (
     <>
-      <div>
-        {/* Header */}
-        <header className="flex justify-between p-5 pt-8 bg-gray-500">
-          {/* Logo and burger */}
-          <a href="/">
-            <img src={logo} alt="" />
-          </a>
-          <button onClick={handleClick}>
-            <img src={iconHamburger} alt="" />
-          </button>
-        </header>
-      </div>
-
-      <Modal modal={modal} handleClick={handleClick} />
+      <div></div>
     </>
   );
 }
